8f0b7bd69a1282d0c8adecb8ccf87ac98cad6e60
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2022-12-05 15:04:30 +0200

    Lesson 3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes/notes");
 const app = express();
 
@@ -14,10 +15,17 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/notes", routes);
 
-// app.get("/", (req, res) => {
-//   res.json({ mssg: "Welcome to the app" });
-// });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
-});
+// mongoDB connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // lsitern for requests
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Connected to MongoDB and listening on port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
